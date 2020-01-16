e120672289bd6e4703e1b4d289038771a6467689
sebastian <xenose@live.com>
2020-01-16 14:24:23 +0900

    to much to fast

--- Menu.js
+++ Menu.js
@@ -18,8 +18,8 @@ function GenMenu()
    
    ulTag.appendChild(CreateLink("ホーム", "Index.html"));
    ulTag.appendChild(CreateLink("事業", "Projects.html"));
+   ulTag.appendChild(CreateLink("モッド", "Mods.html"));
    ulTag.appendChild(CreateLink("ギットハブ", "https://github.com/Xenose?tab=repositories"));
-   ulTag.appendChild(CreateLink("Mods", "Error.html"));
    ulTag.appendChild(CreateLink("僕に就いて", "About.html"));
    ulTag.appendChild(CreateLink("連絡", "Error.html"));
    ulTag.appendChild(CreateLink("About", "Error.html"));
