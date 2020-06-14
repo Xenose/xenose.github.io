47b32be4a433c54357721fd0124ca2c7153fd1f0
Xenose <xenose@live.com>
2020-06-14 21:50:48 +0900

    modified:   index.html 	new file:   terminal.css 	new file:   terminal.js

---
+++ terminal.js
@@ -0,0 +1,23 @@
+
+var commandBufferOutput = "test\ntest\n";
+var commandBufferInput = "";
+
+var canvas = document.getElementById("homeCanvas");
+var d2 = canvas.getContext("2d");
+
+canvas.width = window.innerWidth - 78;
+canvas.height = window.innerHeight;
+
+d2.fillStyle = "#FF0000";
+
+d2.fillText(commandBufferOutput, 15, 200, 100);
+//d2.fillText("[ Github@Xenose ]", 15, 200, 100);
+
+
+function HomeCanvasUpdate()
+{
+}
+
+function NeoFetch()
+{
+}
