dd4aff5d689d0f4f85bfd36943ca4dc76ccae8b5
Xenose <xenose@live.com>
2020-06-19 17:21:12 +0900

    modified:   PostManager.js

--- PostManager.js
+++ PostManager.js
@@ -1,44 +1,37 @@
 "use strict";
 
-//var Projects = document.createElement("Projects");
 
-fetch("posts/astroidWars.json")
-            .then(function(resp2)
+
+function GenratePosts()
+{
+   var Projects = document.createElement("Projects");
+
+   fetch("posts/posts.json")
+      .then(function(resp)
+         {
+            return resp.json();
+         })
+      .then(function(data)
+         {
+            var length = data.postList.length;
+            var i;
+
+            for (i = 0; i < length; i++)
             {
-               console.log(resp2);
-               return resp2.json();
-            })
-            .then(function(data2)
-            {
-               console.log(data2);
-            });
+               var url = "posts/" + data.postList[i];
 
-fetch("posts/posts.json")
-   .then(function(resp)
-   {
-      return resp.json();
-   })
-   .then(function(data)
-   {
-      var length = data.postList.length;
-      var i;
+               fetch(url)
+                  .then(function(resp)
+                     {
+                        return resp.json();
+                     })
+                  .then(function(data)
+                     {
+                        console.log(data2);
 
-      for (i = 0; i < length; i++)
-      {
-         var url = "posts/" + data.postList[i];
-         console.log(url);
 
-         fetch(url)
-            .then(function(resp2)
-            {
-               console.log(resp2);
-               return resp2.json();
-            })
-            .then(function(data2)
-            {
-               console.log(data2);
-            });
-      }
-   });
-
+                     });
+            }
+         });
+}
 
