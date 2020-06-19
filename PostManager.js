e6d10ded65a588cac3d4ae32d893808f191aa774
Xenose <xenose@live.com>
2020-06-19 14:41:38 +0900

    modified:   PostManager.js 	new file:   posts/_AstroidWars.json 	modified:   posts/posts.json

--- PostManager.js
+++ PostManager.js
@@ -9,5 +9,19 @@ fetch("./posts/posts.json")
    })
    .then(function(data)
    {
-      console.log(data);
+      var lenght = data.postList.lenght;
+      var i;
+
+      for (i = 0; i < lenght; i++)
+      {
+         fetch("./posts/" + data.postList[i])
+            .then(function(resp)
+            {
+               return resp.json();
+            })
+            .then(function(data)
+            {
+               console.log(data);
+            });
+      }
    });
