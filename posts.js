61b9dbf68ccbf54fb028f193073a9f55411058e7
Sebastian <xenose@live.com>
2024-02-29 21:01:54 +0900

    testing

--- posts.js
+++ posts.js
@@ -3,7 +3,7 @@ function InsertPosts() {
    const element = document.getElementById("posts");
 
    fetch("https://xenose.github.io/posts.json").then((raw) => {
-      var jsonData = JSON.parse(raw);
+      var jsonData = raw.json();
 
       jsonData.posts.forEach(function (post) {
          element.insertAdjacentHTML('beforeend', "<div><h2>" + post.title + "</h2></div>");
