12749a552d53d37b76fa54c30a67933d68f8a4df
Sebastian <xenose@live.com>
2024-02-29 20:06:27 +0900

    testing

--- posts.js
+++ posts.js
@@ -3,9 +3,11 @@ function InsertPosts() {
    const element = document.getElementById("posts");
 
    fetch("https://xenose.github.io/posts.js").then((jd) => {
-      console.log(jd.json());
+      for (x in jd) {
+         element.innerText = "<div><h2>" + x.title + "</h2></div>";
+      }
    });
 
 
-   element.innerText = "hello";
+
 }
