b40925abc5fc5ab567acc14a1fec2ef13be4e961
Sebastian <xenose@live.com>
2024-02-29 19:41:21 +0900

    testing

--- posts.js
+++ posts.js
@@ -2,7 +2,7 @@
 function InsertPosts() {
    const element = document.getElementById("posts");
 
-   fetch("https://github.com/Xenose/xenose.github.io/blob/master/__posts.json" ,
+   fetch("https://github.com/Xenose/xenose.github.io/blob/master/posts.json" ,
       { headers : {  'Content-Type': 'application/json', 'Accept': 'application/json' }}).then((jd) => {
       console.log(jd.json());
    });
